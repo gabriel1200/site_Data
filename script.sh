39a791b696a456525d07d993b447c6218a27667c
gabriel1200 <grg2117@columbia.edu>
2022-12-07 15:59:48 -0600

    wowy stuff

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@ jupyter nbconvert --to script *.ipynb
 
 python scrape_shooting.py
 python misc.py
-
+python player_level.py
+python underground.py
 git add --all
 git commit -m 'Daily Update'
 git push origin main
