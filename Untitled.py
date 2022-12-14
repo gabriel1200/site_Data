b10f07db2145cbf7c14e559e598f36dd4275612c
gabriel1200 <grg2117@columbia.edu>
2022-12-13 20:58:10 -0600

    Daily Update

---
+++ Untitled.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+df = pd.read_csv('https://raw.githubusercontent.com/gabriel1200/site_Data/main/player_tracking/pullup.csv')
+
+
+# In[6]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
--- player_level.py
+++ player_level.py
@@ -98,11 +98,19 @@ xpath = '//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[2]/div
 path_list = [xpath for i in range(len(url_list))]
 
 
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
 
 tables= get_ptables(url_list,path_list)
 
 
-# In[8]:
+# In[5]:
 
 
 temp = tables[1]
@@ -115,18 +123,11 @@ tables[1] = temp
 tables[1] = temp
 
 
-# In[11]:
+# In[6]:
 
 
 for i in range(len(name_list)):
     tables[i].to_csv('player_tracking/'+name_list[i]+'.csv',index = False)
-
-
-# In[7]:
-
-
-tables[0].to_csv('player_tracking/pullup.csv',index = False)
-tables[1].to_csv('player_tracking/cs.csv',index = False)
 
 
 # In[ ]:
