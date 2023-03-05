307aa473c019682a3c5a578e78af8291d82ab1a3
gabriel1200 <grg2117@columbia.edu>
2023-03-05 01:09:01 -0600

    Daily Update

--- plusmin.py
+++ plusmin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[30]:
 
 
 import pandas as pd
@@ -15,23 +15,121 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementNotInteractableException 
 from selenium.webdriver.support import expected_conditions as EC
-
-
+import os
 import requests
+import time
+import shutil
+import glob
+files = glob.glob('epm/*')
+for f in files:
+    os.remove(f)
+    
+config = configparser.RawConfigParser()
+config.read('conf.cfg')
+    
+details_dict = dict(config.items('LOGIN'))
+
+username = details_dict['username']
+password = details_dict['password']
 
 
-# Step 1: Create a session and load the page
-epm = 'https://dunksandthrees.com/epm?season=2023'
-
-#url_list = [url1,url2,url3,url4,url5]
-
-def check_exists_by_xpath(driver, xpath):
+def load_epm(username,password):
+    options=webdriver.ChromeOptions()
+    # Step 1: Create a session and load the page
+    epm = 'https://dunksandthrees.com/epm'
+    directory =  str(os.getcwd()) +'\epm'
+    pref={"download.default_directory":directory}
+    #example: prefs = {"download.default_directory" : "C:\Tutorial\down"};
+    options.add_experimental_option("prefs",pref)
+    driver = webdriver.Chrome(options=options)
+    driver.get(epm)
     try:
-        driver.find_element(By.XPATH, xpath)
-    except NoSuchElementException:
-        return False
-    return True
-#url_list = [cs,pullup]
+        # find username/email field and send the username itself to the input field
+        driver.find_element("name", "email").send_keys(username)
+        # find password input field and insert password as well
+        driver.find_element("name", "password").send_keys(password)
+        # click login button
+        xpath ='/html/body/div/div[2]/div/div/div[2]/form/div[2]/div[2]/button'
+        wait = WebDriverWait(driver, 10)
+        pause = wait.until(EC.presence_of_element_located((By.XPATH,xpath)))
+
+        driver.find_element(By.XPATH, xpath).click()
+
+        path2 ='/html/body/div/main/div/div[2]/div[1]/div[7]/span/button'
+        pause = wait.until(EC.presence_of_element_located((By.XPATH,path2)))        
+
+        driver.find_element(By.XPATH, path2).click()
+        #url_list = [url1,url2,url3,url4,url5]
+
+        time.sleep(10)
+        #os.remove('epm/epm.csv')
+        os.rename('epm/EPM data.csv','epm/epm.csv')
+        driver.close()
+    except Exception as e:
+        print('Driver Failed')
+load_epm(username,password)
+
+
+# In[32]:
+
+
+def scrape_LEBRON():
+    url = 'https://www.bball-index.com/lebron-database/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    table = soup.find(id = 'table_1')
+    df = pd.read_html(str(table),displayed_only=False)[0]
+    df = df.dropna(subset = 'Player')
+    year = df['Season'].str[0:4].astype(int) +1
+    df['year'] = year
+    df['Player'] = df['Player'].str.lower()
+    df = df.rename(columns = {'Team':'team'})
+    return df
+df = scrape_LEBRON()
+
+
+df = df[df['Season'] =='2022-23']
+df.to_csv('lebron/lebron.csv',index = False)
+df.to_csv('2023/lebron/lebron.csv',index = False)
+
+
+# In[18]:
+
+
+
+
+
+# In[19]:
+
+
+
+
+
+# In[20]:
+
+
+'''
+for i in range(2013,2022):
+        
+
+
+ 
+    path = str(i+1)+'/lebron/'
+    output_dir = Path(path)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    frame = df[df['year'] ==i+1]
+    frame.to_csv(str(i+1)+'/lebron/lebron.csv',index = False)'''
+
+
+# In[7]:
+
+
+df
+
+
+# In[31]:
+
+
 def get_tables(url_list):
     data = []
     driver = webdriver.Chrome()
@@ -89,101 +187,34 @@ def get_tables(url_list):
         data.append(df)
     driver.close()
     return data
-elem = get_tables([epm])
+    df = elem[0]
+    df.columns = df.columns.droplevel()
+    df = df.rename(columns = {'Player':'PLAYER'})
+    df = df.dropna(subset = 'PLAYER')
+    df = df[df.PLAYER != 'PLAYER']
+    df.to_csv('epm_temp.csv',index = False)
+    df = pd.read_csv('epm_temp.csv')
+    new_columns = []
+    columns = df.columns
+    for col in columns:
+       # print(col)
+        #print(df[col].dtype)
+        if df[col].dtype == 'object':
 
+            if '%' in df.loc[1][col]:
+                df[[col,col+'_per']] = df[col].str.split("%",expand=True)
+                df[col] = pd.to_numeric(df[col], errors='coerce')
+                df[col+'_per'] = pd.to_numeric(df[col+'_per'], errors='coerce')
+    #df[['PLAYER','POS']] = df['PLAYER'].str.split('·',expand = True)
+    #df['PLAYER'] = df['PLAYER'].str.replace(' ', '')
+    df['pos'] = df['PLAYER'].str.split("·",expand = True)[1]
 
-# In[2]:
-
-
-elem[0]
-
-
-# In[16]:
-
-
-df = elem[0]
-df.columns = df.columns.droplevel()
-df = df.rename(columns = {'Player':'PLAYER'})
-df = df.dropna(subset = 'PLAYER')
-df = df[df.PLAYER != 'PLAYER']
-df.to_csv('epm_temp.csv',index = False)
-df = pd.read_csv('epm_temp.csv')
-
-
-# In[17]:
-
-
-new_columns = []
-columns = df.columns
-for col in columns:
-   # print(col)
-    #print(df[col].dtype)
-    if df[col].dtype == 'object':
-       
-        if '%' in df.loc[1][col]:
-            df[[col,col+'_per']] = df[col].str.split("%",expand=True)
-            df[col] = pd.to_numeric(df[col], errors='coerce')
-            df[col+'_per'] = pd.to_numeric(df[col+'_per'], errors='coerce')
-#df[['PLAYER','POS']] = df['PLAYER'].str.split('·',expand = True)
-#df['PLAYER'] = df['PLAYER'].str.replace(' ', '')
-df['pos'] = df['PLAYER'].str.split("·",expand = True)[1]
-
-df['PLAYER'] = df['PLAYER'].str.split("·",expand = True)[0]
-df['team'] = df['PLAYER'].str[-4:-1]
-df['PLAYER'] =  df['PLAYER'].str[:-4]
-df = df.round(2)
-df.columns = [x.lower() for x in df.columns]
-
-
-# In[18]:
-
-
-df.to_csv('epm/epm.csv',index = False)
-
-
-# In[19]:
-
-
-def scrape_LEBRON():
-    url = 'https://www.bball-index.com/lebron-database/'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    table = soup.find(id = 'table_1')
-    df = pd.read_html(str(table),displayed_only=False)[0]
-    df = df.dropna(subset = 'Player')
-    year = df['Season'].str[0:4].astype(int) +1
-    df['year'] = year
-    df['Player'] = df['Player'].str.lower()
-    df = df.rename(columns = {'Team':'team'})
-    return df
-df = scrape_LEBRON()
-
-
-df = df[df['Season'] =='2022-23']
-df.to_csv('lebron/lebron.csv',index = False)
-df.to_csv('2023/lebron/lebron.csv',index = False)
-
-
-# In[20]:
-
-
-'''
-for i in range(2013,2022):
-        
-
-
- 
-    path = str(i+1)+'/lebron/'
-    output_dir = Path(path)
-    output_dir.mkdir(parents=True, exist_ok=True)
-    frame = df[df['year'] ==i+1]
-    frame.to_csv(str(i+1)+'/lebron/lebron.csv',index = False)'''
-
-
-# In[7]:
-
-
-df
+    df['PLAYER'] = df['PLAYER'].str.split("·",expand = True)[0]
+    df['team'] = df['PLAYER'].str[-4:-1]
+    df['PLAYER'] =  df['PLAYER'].str[:-4]
+    df = df.round(2)
+    df.columns = [x.lower() for x in df.columns]
+    df.to_csv('epm/epm.csv',index = False)
 
 
 # In[ ]:
