66e12e6edb7fc34e7efa33908aa8c4204040542b
gabriel1200 <grg2117@columbia.edu>
2023-11-21 06:54:01 -0600

    Daily Update

--- underground.py
+++ underground.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
 import pandas as pd
@@ -21,10 +21,15 @@ response = requests.get(url, params=params)
 response_json = response.json()
 player_stats = response_json["multi_row_table_data"]
 df = pd.DataFrame(player_stats)
+#print(df.columns)
 col = ['Name','Minutes','Points','FG2M', 'FG2A', 'FG3M', 'FG3A', 'TsPct','AssistPoints','AtRimAssists','ShortMidRangeAssists', 'LongMidRangeAssists','Corner3Assists','Arc3Assists','LostBallSteals', 'LiveBallTurnovers', 'BadPassOutOfBoundsTurnovers', 'BadPassTurnovers',
-       'DeadBallTurnovers', 'LostBallOutOfBoundsTurnovers', 'LostBallTurnovers', 'StepOutOfBoundsTurnovers', 'Travels', 'Turnovers','OffensiveGoaltends','FTA','OffPoss',
+       'DeadBallTurnovers', 'LostBallOutOfBoundsTurnovers', 'LostBallTurnovers', 'StepOutOfBoundsTurnovers', 'Travels', 'Turnovers','OffensiveGoaltends','FTA','OffPoss','PtsAssisted2s',
+'PtsUnassisted2s',
+'PtsAssisted3s',
+'PtsUnassisted3s',
  'DefPoss','TotalPoss']
 df[col].to_csv('wowy/player_small.csv',index = False)
+#df[col].to_csv('2024/player_tracking/player_small.csv',index = False)
 df.to_csv('wowy/player_large.csv',index = False)
 
 
